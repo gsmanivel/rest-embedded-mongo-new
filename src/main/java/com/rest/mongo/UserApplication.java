f63b71c79241925e2768dd181df2faf83e0593a1
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-05-23 11:34:59 -0400

    part 1

--- src/main/java/com/rest/mongo/UserApplication.java
+++ src/main/java/com/rest/mongo/UserApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class UserExample {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserExample.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 	
 	
